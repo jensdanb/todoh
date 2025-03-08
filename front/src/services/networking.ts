f44f2df5a8d4bb384c6e149e643709fb670cbffe
Your Name <you@example.com>
2025-03-08 16:16:51 +0100

    Started typing services. Typed Networking complete

--- front/src/services/networking.ts
+++ front/src/services/networking.ts
@@ -1,7 +1,9 @@
+import { BackEndPoints, Todo } from "./types";
 
-const hsUrl = 'http://localhost:8080';
 
-const networkErrorResponse = (response) => {
+const hsUrl = 'http://localhost:8080/';
+
+const networkErrorResponse = (response: Response) => {
     return new Response("Network error happened: " + response.status + ' ' + response.statusText, {
         status: 408,
         headers: { "Content-Type": "text/plain" },
@@ -15,7 +17,7 @@ const requestErrorResponse = (suspect='') => {
     });
 };
 
-async function getJSON (address) {
+async function getJSON (address: BackEndPoints) {
     const response = await fetch(hsUrl + address);
     if (!response.ok){
         throw new Error('Network response was not ok')
@@ -24,18 +26,18 @@ async function getJSON (address) {
 };
 
 async function netGetTodos() {
-    return (await getJSON('/getTodos'));
+    return (await getJSON('getTodos'));
 };
 
-async function modifyingQuery (address, clientTodo) {
+async function modifyingQuery (address: BackEndPoints, clientTodo: Todo | [Todo] | string) {
     var method = "";
-    if (["/postTodo", "/postTodos"].includes(address)) {
+    if (["postTodo", "postTodos"].includes(address)) {
         method = "POST"
     }
-    else if (["/putTodo"].includes(address)) {
+    else if (["putTodo"].includes(address)) {
         method = "PUT"
     }
-    else if (["/delTodo"].includes(address)) {
+    else if (["delTodo"].includes(address)) {
         method = "DELETE"
     }
     else {return requestErrorResponse(address)};
@@ -51,30 +53,21 @@ async function modifyingQuery (address, clientTodo) {
     else return response.json();
 };
 
-async function netPostTodo(newTodo) {
-    await modifyingQuery('/postTodo', newTodo);
+async function netPostTodo(newTodo: Todo) {
+    await modifyingQuery('postTodo', newTodo);
 };
 
-async function netPostTodos(newTodos) { // Array
-    return await modifyingQuery('/postTodos', newTodos);
+async function netPostTodos(newTodos: [Todo]) {
+    return await modifyingQuery('postTodos', newTodos);
 };
 
-async function netPutTodo(newTodo) {
-    await modifyingQuery('/putTodo', newTodo);
+async function netPutTodo(newTodo: Todo) {
+    await modifyingQuery('putTodo', newTodo);
 };
 
-async function netDelTodo(id) {
-    await modifyingQuery('/delTodo', id);
+async function netDelTodo(id: string) {
+    await modifyingQuery('delTodo', id);
 };
 
-function setServerData(address, setter) {
-    fetch(hsUrl + address)
-        .then(response => response.json())
-        .then(data => setter( data ))
-        .catch(error => {
-        console.error('Error: ', error)
-        }
-        );
-};
 
 export { getJSON, hsUrl, netGetTodos, netPostTodo, netPostTodos, netPutTodo, netDelTodo };
