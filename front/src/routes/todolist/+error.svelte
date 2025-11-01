b9b60e26109b6dbbe0cde0be5828bcab4120b407
Your Name <you@example.com>
2025-11-01 19:01:35 +0100

    Error UI template

---
+++ front/src/routes/todolist/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let error;
+</script>
+
+<h1>Error</h1>
+<p>{error.message}</p>
+<p>It looks like the item was modified or deleted by another user. Please refresh the page.</p>
+<button on:click={() => window.location.reload()}>Refresh</button>
